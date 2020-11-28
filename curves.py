492b901b46fd0e4ad69d0e118e32c3fade0c6153
giangbang <banggiangle2015@gmail.com>
2020-11-28 10:17:31 +0700

    encrypt by point

--- curves.py
+++ curves.py
@@ -1,34 +1,86 @@
 from points import Points
 import util
+import random
 
-class Curves:
-    secp256k1 = (
-    0,
-    7,
-    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
-    (
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+secp256k1 = (
+    0, #a
+    7, #b
+    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F, #p
+    ( # g
         0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
         0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
-    )
-    )
+    ), 
+    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # n
+)
 
-    def __init__(self, params = Curves.secp256k1):
+class Curves(object):
+
+    def __init__(self, params = secp256k1):
         self.a = params[0]
         self.b = params[1]
         self.p = params[2]
-        if (len(params) == 4):
+        if (len(params) >= 5):
             self.g = Points(params[3][0], params[3][1], self)
+            
+        if (len(params) >= 5):
+            self.n = params[4]
+      
       
     def setG(self, g):
         self.g = g
         
     def getG(self):
         return self.g
+        
+    def getN(self):
+        return self.n;
+        
+    def setN(self, n):
+        self.n = n
+        
+    def __str__(self):
+        return ('a: ' + str(self.a) + '\nb: ' + str(self.b) + '\np: ' + str(self.p) +
+            '\ng: ' + str(self.g) + '\nn: ' + str(self.n))
     
     
+def encrypt(message):
+    res = 0
+    for c in message.lower():
+        res = res * 26 + ord(c) - ord('a')
+    return res
+
+
 if __name__ == '__main__':
     c = Curves()
-    p1 = c.getG()
-    print(type(p1))
-    for i in range(2, 15):
-        print(p1*i)
+    g = c.getG()
+    message = "hello"
+    print("parameters of the elliptic curve are\n" + str(c))
+    na = random.randint(0, c.getN())
+    print('-'*10)
+    print("private key for a is na = " + str(na))
+    pa = na * g
+    print('then, Pa is ' + str(pa))
+    print("the message is " + message)
+    encryptMessage = encrypt(message) % c.getN()
+    print("message is encrypted to " + str(encryptMessage))
+    p = encryptMessage * g
+    print('and is mapped to point: ')
+    print('---\n' +   str(p) + '\n---\n' )
+    k = random.randint(0, c.getN())
+    print('let k be ' + str(k))
+    pc = (k * g, p + k * pa)
+    print('the cipher text pair of points of the message is ' + (str(pc[0])) + ', ' + str(pc[1]))
+    decrypt = pc[1] - (na * pc[0])
+    print('finally, the plainttext point is decrypted as ' )
+    print('---\n' +  str(decrypt)+ '\n---\n' )
--- points.py
+++ points.py
@@ -7,8 +7,9 @@ class Points:
         self.x = a
         self.y = b
         self.c = c
-        if ((self.x**3 + c.a*self.x + c.b - self.y**2)%c.p != 0):
-            raise Exception('not coprime')
+        
+        if (((self.x**3 + c.a*self.x + c.b - self.y**2 + c.p)%c.p) != 0):
+            raise Exception('point not in the curve')
         
     def __add__(self, other):
         if (other == Points.INFINITY):
@@ -38,6 +39,14 @@ class Points:
         
     def __mul__(self, k):
         return Points.mul(self, k)
+        
+    def __sub__(self, other):
+        print(str(other))
+        
+        other.y = other.c.p  - other.y
+        print(str(other))
+        return self + other
+        
         
     def mul(a, b):
         if (b < 0):
